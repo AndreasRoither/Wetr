b66df40364d0cb68d0446db71e315ecfd2b98148
Daniel Englisch <s1610307056@students.fh-hagenberg.at>
2018-10-25 19:35:11 +0200

    Restructure project

---
+++ sql/create.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `persons` (
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(60) NOT NULL
+);
---
+++ sql/insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO `persons` (`id`, `name`) VALUES
+(1, 'Daniel'),
+(2, 'Andi');
+
+COMMIT;
