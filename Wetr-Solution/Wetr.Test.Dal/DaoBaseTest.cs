9ac615bf6629b8ef1408bb115078426d833d8f41
Daniel Englisch <s1610307056@students.fh-hagenberg.at>
2018-11-05 19:09:43 +0100

    Add test setup and cleanup method for managing transactions

--- Wetr-Solution/Wetr.Test.Dal/DaoBaseTest.cs
+++ Wetr-Solution/Wetr.Test.Dal/DaoBaseTest.cs
@@ -4,19 +4,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace Wetr.Test.Dal
 {
     public abstract class DaoBaseTest
     {
+        private TransactionScope transaction;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            transaction = new TransactionScope();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            transaction.Dispose();
+        }
+
+        [TestMethod]
         public abstract Task TestFindByIdAsync();
 
+        [TestMethod]
         public abstract Task TestFindAllAsync();
 
+        [TestMethod]
         public abstract Task TestUpdateAsync();
 
+        [TestMethod]
         public abstract Task TestDeleteAsync();
 
+        [TestMethod]
         public abstract Task TestInsertAsync();
     }
 }
