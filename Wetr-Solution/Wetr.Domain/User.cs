9b48e2e5c99ba1f4d51062cf52fbf8b5b9b71a06
Daniel Englisch <s1610307056@students.fh-hagenberg.at>
2018-11-05 19:09:01 +0100

    Override Equals method

--- Wetr-Solution/Wetr.Domain/User.cs
+++ Wetr-Solution/Wetr.Domain/User.cs
@@ -8,6 +8,20 @@
         public string Password { get; set; }
         public string Email { get; set; }
 
+        public override bool Equals(object obj)
+        {
+            if (!(obj is User))
+                return false;
+
+            User u = obj as User;
+
+            return u.UserId == UserId &&
+                u.FirstName.Equals(FirstName) && 
+                u.LastName.Equals(LastName) && 
+                u.Password.Equals(Password) &&
+                u.Email.Equals(Email);
+        }
+
         public override string ToString() =>
             $"[{UserId}] {FirstName} {LastName} {Password} {Email}";
     }
