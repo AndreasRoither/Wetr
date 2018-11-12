673cf74eedebc8779db931469f188dd488413ea7
AndiRoither <andi.roither@protonmail.com>
2018-11-12 14:41:17 +0100

    Add Thread Safe Singleton AdoFactory + Interface

---
+++ Wetr-Solution/Wetr.Dal.Factory/AdoFactory.cs
@@ -0,0 +1,92 @@
+﻿using Common.Dal.Ado;
+using Wetr.Dal.Ado;
+using Wetr.Dal.Interface;
+
+namespace Wetr.Dal.Factory
+{
+    public sealed class AdoFactory : IDaoFactory
+    {
+        private const string connectionStringConfigName_factory = "WETR-Testing";
+
+        private static AdoFactory instance = null;
+        private static readonly object padlock = new object();
+
+        // thread safe singleton
+        public static AdoFactory Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new AdoFactory();
+                    }
+                    return instance;
+                }
+            }
+
+            private set { }
+        }
+
+        public IAddressDao GetAddressDao(string connectionStringConfigName = connectionStringConfigName_factory)
+        {
+            return new AdoAddressDao(DefaultConnectionFactory.FromConfiguration(connectionStringConfigName));
+        }
+
+        public ICommunityDao GetCommunityDao(string connectionStringConfigName = connectionStringConfigName_factory)
+        {
+            return new AdoCommunityDao(DefaultConnectionFactory.FromConfiguration(connectionStringConfigName));
+        }
+
+        public ICountryDao GetCountryDao(string connectionStringConfigName = connectionStringConfigName_factory)
+        {
+            return new AdoCountryDao(DefaultConnectionFactory.FromConfiguration(connectionStringConfigName));
+        }
+
+        public IDistrictDao GetDistrictDao(string connectionStringConfigName = connectionStringConfigName_factory)
+        {
+            return new AdoDistrictDao(DefaultConnectionFactory.FromConfiguration(connectionStringConfigName));
+        }
+
+        public IMeasurementDao GetMeasurementDao(string connectionStringConfigName = connectionStringConfigName_factory)
+        {
+            return new AdoMeasurementDao(DefaultConnectionFactory.FromConfiguration(connectionStringConfigName));
+        }
+
+        public IMeasurementTypeDao GetMeasurementTypeDao(string connectionStringConfigName = connectionStringConfigName_factory)
+        {
+            return new AdoMeasurementTypeDao(DefaultConnectionFactory.FromConfiguration(connectionStringConfigName));
+        }
+
+        public IPermissionDao GetPermissionDao(string connectionStringConfigName = connectionStringConfigName_factory)
+        {
+            return new AdoPermissionDao(DefaultConnectionFactory.FromConfiguration(connectionStringConfigName));
+        }
+
+        public IProvinceDao GetProvinceDao(string connectionStringConfigName = connectionStringConfigName_factory)
+        {
+            return new AdoProvinceDao(DefaultConnectionFactory.FromConfiguration(connectionStringConfigName));
+        }
+
+        public IStationDao GetStationDao(string connectionStringConfigName = connectionStringConfigName_factory)
+        {
+            return new AdoStationDao(DefaultConnectionFactory.FromConfiguration(connectionStringConfigName));
+        }
+
+        public IStationTypeDao GetStationTypeDao(string connectionStringConfigName = connectionStringConfigName_factory)
+        {
+            return new AdoStationTypeDao(DefaultConnectionFactory.FromConfiguration(connectionStringConfigName));
+        }
+
+        public IUnitDao GetUnitDao(string connectionStringConfigName = connectionStringConfigName_factory)
+        {
+            return new AdoUnitDao(DefaultConnectionFactory.FromConfiguration(connectionStringConfigName));
+        }
+
+        public IUserDao GetUserDao(string connectionStringConfigName = connectionStringConfigName_factory)
+        {
+            return new AdoUserDao(DefaultConnectionFactory.FromConfiguration(connectionStringConfigName));
+        }
+    }
+}
---
+++ Wetr-Solution/Wetr.Dal.Factory/IDaoFactory.cs
@@ -0,0 +1,20 @@
+﻿using Wetr.Dal.Interface;
+
+namespace Wetr.Dal.Factory
+{
+    interface IDaoFactory
+    {
+        IAddressDao           GetAddressDao(string connectionStringConfigName);
+        ICommunityDao         GetCommunityDao(string connectionStringConfigName);
+        ICountryDao           GetCountryDao(string connectionStringConfigName);
+        IDistrictDao          GetDistrictDao(string connectionStringConfigName);
+        IMeasurementDao       GetMeasurementDao(string connectionStringConfigName);
+        IMeasurementTypeDao   GetMeasurementTypeDao(string connectionStringConfigName);
+        IPermissionDao        GetPermissionDao(string connectionStringConfigName);
+        IProvinceDao          GetProvinceDao(string connectionStringConfigName);
+        IStationDao           GetStationDao(string connectionStringConfigName);
+        IStationTypeDao       GetStationTypeDao(string connectionStringConfigName);
+        IUnitDao              GetUnitDao(string connectionStringConfigName);
+        IUserDao              GetUserDao(string connectionStringConfigName);
+    }
+}
