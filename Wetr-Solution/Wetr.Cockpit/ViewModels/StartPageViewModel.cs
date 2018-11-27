3fa8a25269bfe49980f4ece7d8945b05fcb891ab
Andreas Roither <andi.roither@protonmail.com>
2018-11-27 20:27:33 +0100

    Remove WPF, Add UWP
    Set Debug to auto deploy (otherwise changes will not be seen)

---
+++ Wetr-Solution/Wetr.Cockpit/ViewModels/StartPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace Wetr.Cockpit.ViewModels
+{
+    public class StartPageViewModel : ViewModelBase
+    {
+        private string _helloWorld;
+
+        public string HelloWorld
+        {
+            get
+            {
+                return _helloWorld;
+            }
+            set
+            {
+                Set(() => HelloWorld, ref _helloWorld, value);
+            }
+        }
+
+        public StartPageViewModel()
+        {
+            HelloWorld = IsInDesignMode
+                ? "Runs in design mode"
+                : "Runs in runtime mode";
+        }
+    }
+}
