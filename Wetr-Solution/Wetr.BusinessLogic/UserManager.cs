41a6618087b6ba079aa1408e6f413632f914ea65
Andreas Roither <andi.roither@protonmail.com>
2018-12-16 18:45:49 +0100

    Add UserManager Logic

--- Wetr-Solution/Wetr.BusinessLogic/UserManager.cs
+++ Wetr-Solution/Wetr.BusinessLogic/UserManager.cs
@@ -1,23 +1,47 @@
 ﻿
+using Wetr.Dal.Factory;
+using Wetr.Dal.Interface;
+using Wetr.Domain;
+
 namespace Wetr.BusinessLogic
 {
     public class UserManager
     {
+        IUserDao userDao;
+
         public UserManager(string databaseName)
         {
+            userDao = AdoFactory.Instance.GetUserDao(databaseName);
         }
 
         #region functions
 
         public bool UserCredentialValidation(string email, string password)
         {
+            User user = userDao.FindByEmailAsync(email).Result;
+            return BCrypt.Net.BCrypt.Verify(password, user.Password);
+
             // hash and save a password
-            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+            //string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
 
             // check a password
-            bool validPassword = BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+            //bool validPassword = BCrypt.Net.BCrypt.Verify(password, hashedPassword);
 
             // BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool RegisterUser(User user)
+        {
+            if (!CheckUser(user)) return false;
+            return userDao.InsertAsync(user).Result;
+        }
+
+        public bool CheckUser(User user)
+        {
+            if (user.Email.Equals(string.Empty)) return false;
+            if (user.FirstName.Equals(string.Empty)) return false;
+            if (user.LastName.Equals(string.Empty)) return false;
+            if (user.Password.Equals(string.Empty)) return false;
             return true;
         }
 
