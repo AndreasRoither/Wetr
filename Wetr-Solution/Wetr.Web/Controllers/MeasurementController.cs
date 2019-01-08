d2c374b1b5da44f1a84cfb3cbd99ebba718b0463
Daniel Englisch <s1610307056@students.fh-hagenberg.at>
2019-01-08 20:28:12 +0100

    Add route for posting measurements from stations

---
+++ Wetr-Solution/Wetr.Web/Controllers/MeasurementController.cs
@@ -0,0 +1,54 @@
+﻿using Swashbuckle.Examples;
+using Swashbuckle.Swagger.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using Wetr.Dal.Factory;
+using Wetr.Dal.Interface;
+using Wetr.Domain;
+using Wetr.Web.BL;
+using Wetr.Web.DTOs;
+using Wetr.Web.Requests;
+using Wetr.Web.Responses;
+
+namespace Wetr.Web.Controllers
+{
+    [RoutePrefix("v1/measurements")]
+    public class MeasurementController : ApiController
+    {
+
+        [Route("")]
+        [HttpPost]
+        [JWT]
+
+        [SwaggerResponse(HttpStatusCode.Unauthorized, "Invalid Authorization header.", null)]
+        [SwaggerResponse(HttpStatusCode.BadRequest, "Invalid json format or invalid request body.", typeof(Dictionary<string, string[]>))]
+        [SwaggerResponse(HttpStatusCode.OK, "Measurement was posted successful.ly", null)]
+
+        public async Task<IHttpActionResult> PostMeasurement(MeasurementDTO measurement)
+        {
+
+            /* Check if model is valid */
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState.ToDictionary(
+                                                             kvp => kvp.Key,
+                                                             kvp => kvp.Value.Errors.Select(e => e.ErrorMessage).ToArray()
+                               );
+                return Content(HttpStatusCode.BadRequest, errors);
+            }
+
+            IMeasurementDao dao = AdoFactory.Instance.GetMeasurementDao("wetr");
+            await dao.InsertAsync(measurement.ToMeasurement());
+
+            return Content(HttpStatusCode.OK, new object());
+        }
+
+
+    }
+}
---
+++ Wetr-Solution/Wetr.Web/DTOs/MeasurementDTO.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Wetr.Domain;
+
+namespace Wetr.Web.DTOs
+{
+    public class MeasurementDTO
+    {
+        [Range(1,int.MaxValue)]
+        public int StationId { get; set; }
+
+        [Range(double.MinValue, double.MaxValue)]
+        public double Value { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int MeasurementTypeId { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int MeasurementId { get; set; }
+
+
+        [Range(1, int.MaxValue)]
+        public int UnitId { get; set; }
+
+        [Required]
+        public DateTime TimesStamp { get; set; }
+
+        public Measurement ToMeasurement()
+        {
+            return new Measurement()
+            {
+                MeasurementId = 0,
+                MeasurementTypeId = this.MeasurementTypeId,
+                StationId = this.StationId,
+                TimesStamp = this.TimesStamp,
+                UnitId = this.UnitId,
+                Value = this.Value
+            };
+        }
+
+    }
+}
