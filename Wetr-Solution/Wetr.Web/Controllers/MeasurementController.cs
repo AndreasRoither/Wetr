1c672d15073238c3c6263fcfefb62cb694030f6e
Daniel Englisch <s1610307056@students.fh-hagenberg.at>
2019-01-11 17:43:53 +0100

    Add ability to query measurement data for a specific station

--- Wetr-Solution/Wetr.Web/Controllers/MeasurementController.cs
+++ Wetr-Solution/Wetr.Web/Controllers/MeasurementController.cs
@@ -10,8 +10,10 @@ using System.Web.Http;
 using System.Web.Http.Cors;
 using Wetr.Dal.Factory;
 using Wetr.Dal.Interface;
+using Wetr.Domain;
 using Wetr.Web.BL;
 using Wetr.Web.DTOs;
+using Wetr.Web.Requests;
 
 namespace Wetr.Web.Controllers
 {
@@ -45,5 +47,35 @@ namespace Wetr.Web.Controllers
 
             return Content(HttpStatusCode.OK, new object());
         }
+
+        [Route("query")]
+        [HttpPost]
+        [JWT]
+
+        [SwaggerResponse(HttpStatusCode.Unauthorized, "Invalid Authorization header.", null)]
+        [SwaggerResponse(HttpStatusCode.BadRequest, "Invalid json format or invalid request body.", typeof(Dictionary<string, string[]>))]
+        [SwaggerResponse(HttpStatusCode.OK, "Measurement data of query.", typeof(double[]))]
+
+        public async Task<IHttpActionResult> QueryMeasurements(QueryRequest query)
+        {
+            /* Check if model is valid */
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState.ToDictionary(
+                                                             kvp => kvp.Key,
+                                                             kvp => kvp.Value.Errors.Select(e => e.ErrorMessage).ToArray()
+                               );
+                return Content(HttpStatusCode.BadRequest, errors);
+            }
+
+            IMeasurementDao dao = AdoFactory.Instance.GetMeasurementDao("wetr");
+            IStationDao stationDao = AdoFactory.Instance.GetStationDao("wetr");
+            Station s = await stationDao.FindByIdAsync(query.StationId);
+            double[] result = await dao.GetQueryResult(query.Start, query.End, query.MeasurementTypeId, query.ReductionTypeId, query.GroupingTypeId,
+                new List<Station>() { s });
+
+            return Content(HttpStatusCode.OK, result);
+        }
+
     }
 }
---
+++ Wetr-Solution/Wetr.Web/Requests/QueryRequest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Wetr.Web.Requests
+{
+    public class QueryRequest
+    {
+        [Range(0,5)]
+        public int MeasurementTypeId { get; set; }
+
+        [Range(0, 4)]
+        public int GroupingTypeId { get; set; }
+
+        [Range(0, 3)]
+        public int ReductionTypeId { get; set; }
+
+        [Required]
+        public DateTime Start { get; set; }
+
+        [Required]
+        public DateTime End { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int StationId { get; set; }
+
+    }
+}
